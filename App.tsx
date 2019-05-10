3cd24a059da31f81c71775cc6e32097295a85617
Abdelrahman Maged <abdulrahman.maged95@gmail.com>
2019-05-11 00:31:12 +0200

    :building_construction: Migrate to TypeScript

---
+++ App.tsx
@@ -0,0 +1,78 @@
+import * as React from 'react';
+import { View, Text, LayoutAnimation, StyleSheet } from 'react-native';
+import { Permissions, BarCodeScanner, BarCodeScannedCallback } from 'expo';
+import { parse } from 'url-otpauth';
+
+type Props = {};
+
+type State = {
+  hasCameraPermission: boolean | null;
+  scannedUrl: string | null;
+};
+
+export default class App extends React.Component<Props, State> {
+  state: State = {
+    hasCameraPermission: null,
+    scannedUrl: null,
+  };
+
+  componentDidMount() {
+    this._requestCameraPermission();
+  }
+
+  _requestCameraPermission = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+
+    this.setState({
+      hasCameraPermission: status === 'granted',
+    });
+  };
+
+  _handleBarCodeScan: BarCodeScannedCallback = result => {
+    if (result.data !== this.state.scannedUrl) {
+      LayoutAnimation.spring();
+
+      const { data: scannedUrl } = result;
+
+      this.setState({ scannedUrl });
+
+      console.log('SCANNED URL >', scannedUrl);
+      console.log('PARSED SCANNED URL >', parse(scannedUrl));
+    }
+  };
+
+  render() {
+    const { hasCameraPermission } = this.state;
+
+    if (hasCameraPermission === null) {
+      return (
+        <View style={styles.container}>
+          <Text>Requesting for camera permission</Text>
+        </View>
+      );
+    }
+
+    if (hasCameraPermission === false) {
+      return (
+        <View style={styles.container}>
+          <Text>Camera permission is not granted</Text>
+        </View>
+      );
+    }
+
+    return (
+      <BarCodeScanner
+        style={styles.container}
+        onBarCodeScanned={this._handleBarCodeScan}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});

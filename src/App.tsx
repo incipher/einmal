a86a666393f2ec42e117da80d08eaa723ea75e96
Abdelrahman Abdelhafez <abdelrahman@maged.rocks>
2020-03-08 12:40:45 +0100

    :recycle: Extract TOTP generation logic to authenticatorToken()

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-global.Buffer = global.Buffer || require('buffer').Buffer;
-
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { totpToken, totpOptions, KeyEncodings } from '@otplib/core';
-import { createDigest } from '@otplib/plugin-crypto-js';
-import { keyDecoder } from '@otplib/plugin-base32-enc-dec';
+import { authenticatorToken } from './crypto';
 
 export default function App() {
   const [token, setToken] = useState('');
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      const secret = 'CFNMN7VBAIC5XYVG';
-
-      const totp = totpToken(
-        keyDecoder(secret, KeyEncodings.HEX),
-        totpOptions({
-          createDigest,
-          encoding: KeyEncodings.HEX,
-        }),
-      );
-
+      const totp = authenticatorToken('CFNMN7VBAIC5XYVG');
       setToken(totp);
     }, 1000);
 

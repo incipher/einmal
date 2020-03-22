7890957ade5ede8ee5335a416bc888b6355462a3
Abdelrahman Abdelhafez <abdelrahman@maged.rocks>
2020-03-22 04:34:58 +0100

    :children_crossing: Specify how SafeAreaView & StatusBar should behave

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
 import { ThemeProvider, DarkTheme, Theme } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -18,26 +19,48 @@ const Stack = createStackNavigator();
 
 const App: React.FC = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStateProvider>
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName="Welcome">
-            <Stack.Screen
-              name="Welcome"
-              component={Welcome}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="SetupAuthentication"
-              component={SetupAuthentication}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen name="Home" component={TokensList} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </GlobalStateProvider>
-    </ThemeProvider>
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="light-content" />
+
+      <ThemeProvider theme={theme}>
+        <GlobalStateProvider>
+          <NavigationContainer>
+            <Stack.Navigator initialRouteName="Welcome">
+              <Stack.Screen
+                name="Welcome"
+                component={Welcome}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="SetupAuthentication"
+                component={SetupAuthentication}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="Home"
+                component={TokensList}
+                options={{
+                  headerTitle: 'Einmal',
+                  headerStyle: {
+                    backgroundColor: 'black',
+                  },
+                  headerTitleStyle: {
+                    color: 'white',
+                  },
+                }}
+              />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </GlobalStateProvider>
+      </ThemeProvider>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;

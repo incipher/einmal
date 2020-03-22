53d3cea08292963266a0de018db654e0d4dc03d2
Abdelrahman Abdelhafez <abdelrahman@maged.rocks>
2020-03-22 04:12:05 +0100

    :lipstick: Activate dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,37 @@
 import React from 'react';
-import { ThemeProvider, DefaultTheme } from 'react-native-paper';
+import { ThemeProvider, DarkTheme, Theme } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { TokensList } from './screens';
+import { Welcome, SetupAuthentication, TokensList } from './screens';
 import { GlobalStateProvider } from './hooks';
+
+const theme: Theme = {
+  ...DarkTheme,
+  colors: {
+    ...DarkTheme.colors,
+    primary: 'white',
+    background: 'black',
+  },
+};
 
 const Stack = createStackNavigator();
 
 const App: React.FC = () => {
   return (
-    <ThemeProvider theme={DefaultTheme}>
+    <ThemeProvider theme={theme}>
       <GlobalStateProvider>
         <NavigationContainer>
-          <Stack.Navigator initialRouteName="Home">
+          <Stack.Navigator initialRouteName="Welcome">
+            <Stack.Screen
+              name="Welcome"
+              component={Welcome}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="SetupAuthentication"
+              component={SetupAuthentication}
+              options={{ headerShown: false }}
+            />
             <Stack.Screen name="Home" component={TokensList} />
           </Stack.Navigator>
         </NavigationContainer>
--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,1 +1,3 @@
+export { default as Welcome } from './Welcome';
+export { default as SetupAuthentication } from './SetupAuthentication';
 export { default as TokensList } from './TokensList';

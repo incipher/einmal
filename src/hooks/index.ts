c5927b88e9d0adde71fe288672158bd20fd8acd5
Abdelrahman Abdelhafez <abdelrahman@maged.rocks>
2020-04-14 03:54:08 +0200

    :sparkles: Implement usePrevious() hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,3 @@
 export { useGlobalState, GlobalStateProvider } from './useGlobalState';
 export { useInteractables, InteractablesProvider } from './useInteractables';
+export { default as usePrevious } from './usePrevious';

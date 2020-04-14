ad0856677dae1e0adf533c3670540a0f3eb7cc11
Abdelrahman Abdelhafez <abdelrahman@maged.rocks>
2020-04-15 01:54:19 +0200

    :fire: Remove unused import

--- src/components/Token.tsx
+++ src/components/Token.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text, Avatar, TouchableRipple } from 'react-native-paper';
-import Animated, {
+import {
   set,
   interpolate,
   concat,

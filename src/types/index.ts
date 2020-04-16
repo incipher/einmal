b4a1294e7f0d45994e0f187752c90bfeb27b14df
Abdelrahman Abdelhafez <abdelrahman@maged.rocks>
2020-04-16 03:16:47 +0200

    :label: Define SectionList types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,18 @@
+/* React Native */
+
+// SectionList
+
+export type Section<T> = {
+  title: string;
+  data: T[];
+};
+
+export type Sections<T> = Section<T>[];
+
+/* Global State */
+
+// Vault
+
 export type Vault = VaultEntry[];
 
 export type VaultEntry = {

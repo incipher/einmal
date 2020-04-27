dc2fa830568211f6dfc42bcc2ea9fe1666d796ba
Abdelrahman Abdelhafez <abdelrahman@maged.rocks>
2020-04-27 03:59:48 +0200

    :robot: Fix splash screen on Android

--- android/app/src/main/java/io/incipher/einmal/MainActivity.java
+++ android/app/src/main/java/io/incipher/einmal/MainActivity.java
@@ -1,11 +1,25 @@
 package io.incipher.einmal;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
+import expo.modules.splashscreen.SplashScreen;
+import expo.modules.splashscreen.SplashScreenImageResizeMode;
+
 public class MainActivity extends ReactActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SplashScreen.show(
+            this,
+            SplashScreenImageResizeMode.CONTAIN,
+            ReactRootView.class
+        );
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.

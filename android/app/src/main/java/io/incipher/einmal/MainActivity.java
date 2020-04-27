6ae7bd4a1b0a64bdf6da84a8bf1d761799f32a11
Abdelrahman Abdelhafez <abdelrahman@maged.rocks>
2020-04-27 05:14:32 +0200

    :lock: Employ security screen on Android

--- android/app/src/main/java/io/incipher/einmal/MainActivity.java
+++ android/app/src/main/java/io/incipher/einmal/MainActivity.java
@@ -1,6 +1,7 @@
 package io.incipher.einmal;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -14,6 +15,12 @@ public class MainActivity extends ReactActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_SECURE,
+            WindowManager.LayoutParams.FLAG_SECURE
+        );
+
         SplashScreen.show(
             this,
             SplashScreenImageResizeMode.CONTAIN,

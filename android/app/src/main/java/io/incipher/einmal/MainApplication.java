2ea1e46b64457a7bfdb7533a140dc4dc004294bd
Abdelrahman Abdelhafez <abdelrahman@maged.rocks>
2020-04-27 04:45:41 +0200

    :heavy_plus_sign: Install react-native-safe-area-context

--- android/app/src/main/java/io/incipher/einmal/MainApplication.java
+++ android/app/src/main/java/io/incipher/einmal/MainApplication.java
@@ -9,6 +9,7 @@ import android.content.Context;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;

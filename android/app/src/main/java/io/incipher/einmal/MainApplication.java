acf04cd191b91c5345b5e192d54417e23d146be9
Abdelrahman Abdelhafez <abdelrahman@maged.rocks>
2020-04-29 00:45:20 +0200

    :heavy_plus_sign: Install react-native-fs

--- android/app/src/main/java/io/incipher/einmal/MainApplication.java
+++ android/app/src/main/java/io/incipher/einmal/MainApplication.java
@@ -9,7 +9,6 @@ import android.content.Context;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;

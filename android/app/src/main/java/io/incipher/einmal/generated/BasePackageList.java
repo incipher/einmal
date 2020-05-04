458749386c865cdb13e0a8c3e40129ea605a5599
Abdelrahman Abdelhafez <abdelrahman@maged.rocks>
2020-05-04 20:38:36 +0200

    :heavy_plus_sign: Install expo-secure-store

--- android/app/src/main/java/io/incipher/einmal/generated/BasePackageList.java
+++ android/app/src/main/java/io/incipher/einmal/generated/BasePackageList.java
@@ -17,9 +17,11 @@ public class BasePackageList {
         new expo.modules.imageloader.ImageLoaderPackage(),
         new expo.modules.keepawake.KeepAwakePackage(),
         new expo.modules.lineargradient.LinearGradientPackage(),
+        new expo.modules.localauthentication.LocalAuthenticationPackage(),
         new expo.modules.location.LocationPackage(),
         new expo.modules.permissions.PermissionsPackage(),
         new expo.modules.random.RandomPackage(),
+        new expo.modules.securestore.SecureStorePackage(),
         new expo.modules.splashscreen.SplashScreenPackage(),
         new expo.modules.sqlite.SQLitePackage(),
         new expo.modules.webbrowser.WebBrowserPackage()
